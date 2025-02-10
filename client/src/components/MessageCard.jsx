3cef54d36a315304ab14e3150e0761be0aa6860d
jochris15 <jochris15@gmail.com>
2025-02-10 17:34:49 +0700

    change msg

--- client/src/components/MessageCard.jsx
+++ client/src/components/MessageCard.jsx
@@ -5,19 +5,19 @@ const MessageCard = () => {
     const [fade, setFade] = useState(false);
 
     const loveNotes = [
-        "Are you a magician? Because whenever I look at you, everyone else disappears!",
-        "I must be a snowflake because I’ve fallen for you!",
-        "Is your name Google? Because you've got everything I've been searching for!",
-        "If you were a vegetable, you'd be a cute-cumber!",
+        "Are you a magician? Because whenever I look at you, everyone else disappears",
+        "I must be a snowflake because I’ve fallen for you",
+        "Is your name Google? Because you've got everything I've been searching for",
+        "If you were a vegetable, you'd be a cute-cumber",
         "Are you tired? Because you've been running through my mind all day. And probably all night too.",
         "Do you have a map? Because I keep getting lost in your eyes.",
         "If you were a superhero, what would your power be? My guess is making me smile",
         "Are you a time traveler? Because you make every moment feel special",
         "If bring treats, will Maple let me hang out with you?",
         "I'm not sure what's sweeter, you or Lessy's puppy dog eyes.",
-        "I'm not sure if I'm more excited to see you or Maple. Just kidding...mostly.",
+        "I'm not sure if I'm more excited to see you or Maple. Just kidding, mostly.",
         "I'm not sure who's cuter, you or Lessy. It's a tough call.",
-        "I'm barking mad about you...just like Lessy when she sees a cat."
+        "I'm barking mad about you, just like Lessy when she sees a cat."
     ];
 
     const revealMessage = () => {
